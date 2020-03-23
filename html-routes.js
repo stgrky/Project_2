3a1d69862f49c8ff953e289c09f5ca1d21cdd67a
Kendra Kwoka <57764303+kkwoka@users.noreply.github.com>
2020-03-22 22:19:30 -0500

    Add files via upload

---
+++ html-routes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+// Routes
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+
+};
