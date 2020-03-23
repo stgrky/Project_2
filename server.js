2ba9924f92dd223f56b534ec048274bd1741f1db
Kendra Kwoka <57764303+kkwoka@users.noreply.github.com>
2020-03-22 22:16:04 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
+// Dependencies
 // =============================================================
 var express = require("express");
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
 // Requiring our models for syncing
 var db = require("./models");
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Static directory
 app.use(express.static("public"));
 
+
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./routes/doctor-api-routes")
+// (app);
+require("./routes/html-routes")
+// (app);
+require("./routes/patients-api-routes")
+// (app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
