08f1347bd601f14c39117dc481857ce762c44714
Kendra Kwoka <57764303+kkwoka@users.noreply.github.com>
2020-03-22 22:18:20 -0500

    Add files via upload

---
+++ patient.js
@@ -0,0 +1,51 @@
+module.exports = function(sequelize, DataTypes) {
+    let Patients = sequelize.define("Patients", {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        validate: {
+            len: [1]
+        }
+      },
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+      },
+      phone_number: {
+          type: DataTypes.INTEGER,
+          len: [0, 11]
+      },
+      city: DataTypes.STRING,
+      symptoms: DataTypes.STRING,
+      infected: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false
+      },
+      treatment: DataTypes.STRING,
+      admitted: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false
+      }, 
+      doctor: DataTypes.STRING,
+    //   createdAt: DataTypes.DATETIME
+    });
+
+    Patients.associate = function (models) {
+        // a Patient should belong to an Doctor
+        // A Patient can't be created without a Doctor due to the foreign key constraint
+        Patients.belongsTo(models.Doctor, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Patients;
+};
+
+// foreign key (doctor_id) references doctors(doctor_id)  
