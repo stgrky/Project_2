ecca857cbe9ee895a7dadeeaa13e3f816113d976
Kendra Kwoka <kendrakwoka@gmail.com>
2020-03-24 10:12:27 -0500

    added lame template to index.html to test if its working

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ db.sequelize.sync({ force: false }).then(function() {
 
 // -------
 
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var mysql = require('mysql');
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
-});
+//   console.log('The solution is: ', rows[0].solution);
+// });
 
-connection.end();
+// connection.end();
