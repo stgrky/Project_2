5a66dbd1ad8c6eb48b914f5d2a6424302ce2f4c6
Kendra Kwoka <kendrakwoka@gmail.com>
2020-03-27 09:43:46 -0500

    changed SERVE to START so both servers would start together!

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
+// const serveStatic = require("serve-static");
+// const path = require("path");
 
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-
-// Requiring our models for syncing
-var db = require("./models");
-
+let app = express();
+let PORT = process.env.PORT || 8081;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+// here we are configuring dist to serve app files
+// app.use("/", serveStatic(path.join(_dirname, "/dist")));
+
+// this * route is to serve project on different page routes except root '/'
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(path.join(_dirname, "/dist/index.html"));
+// });
+
+
+// Requiring our models for syncing
+let db = require("./models");
+
+
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("dist"));
 
 
 // Routes
 // =============================================================
 require("./routes/doctor-api-routes")(app);
-require("./routes/html-routes")(app);
+// require("./routes/html-routes")(app);
 require("./routes/patients-api-routes")(app);
 
 
