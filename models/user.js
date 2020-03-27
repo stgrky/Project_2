3e55b19d340fb02ccd7f2e5b4682d7896f94e426
Kendra Kwoka <kendrakwoka@gmail.com>
2020-03-27 12:11:47 -0500

    uncommented out bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-// var bcrypt = require("bcryptjs");
+var bcrypt = require("bcryptjs");
 
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
