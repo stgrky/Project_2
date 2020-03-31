2249e4dab79b685071a026eac1460034ef7d30ee
Leandra Turner <calliet0715@gmail.com>
2020-03-31 18:45:55 -0500

    Addition of logout route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import login from "../components/login.vue";
 import secure from "../components/secure.vue";
 import new_patient from "../components/new_patient.vue";
+import logout from "../components/logout.vue";
 
 export const routes= [
     {path: "/", component: login, name: "login"}, // getting error with this pathway!
     {path: "/secure", component: secure, name: "secure" },
-    {path: "/new_patient", component: new_patient, name: "new_patient"}
+    {path: "/new_patient", component: new_patient, name: "new_patient"},
+    {path: "/logout", component: logout, name: "logout"}
 ];
