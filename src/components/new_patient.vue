1a715b937a0046750ae6e87ba8b5c6b9f8f27451
Grant Kyle <sgrantkyle@gmail.com>
2020-03-31 01:26:52 -0500

    new patient page almost done. formatting error to figure out

--- src/components/new_patient.vue
+++ src/components/new_patient.vue
@@ -1,106 +1,118 @@
 <template>
-  <div id="secure">
-    <h1>Patient List - {{ search }}</h1>
-    <h2>Search Your Patient Below</h2>
+  <!-- Adding New Patient -->
+  <div class="container">
+    <div class="row justify-center-content">
+      <div class="col-md=8">
+        <div class="card">
+          <div class="card-header">Add a Patient - {{ addPatient.name }}</div>
 
-    <input id="bubbles" type="text" v-model="search" placeholder="Patient Name" />
-    <button id="bubbles-two" type="button" v-on:click="patientSearch()">Search</button>
-
-    <!-- Adding New Patient -->
-    <div class="container">
-        <div class="row justify-center-content">
-            <div class="card">
-    <div class="card-header">Add a Patient - {{ addPatient.name }}</div>
-
-    <div class="card-body">
-        <form @submit="addPatient">
-    <input id="bubbles-three" type="text" v-model="name" placeholder="Patient Name" />
-    <input id="bubbles-three" type="text" v-model="phone_number" placeholder="Phone Number" />
-    <input id="bubbles-three" type="text" v-model="city" placeholder="City" />
-    <input id="bubbles-three" type="text" v-model="symptoms" placeholder="Symptoms" />
-    <div class="form-questions">
-      <label for="admittedQuestion">Infected?</label>
-      <input type="radio" id="yes" value="Yes" v-model="infectedQuestion" /> Yes
-      <input type="radio" id="no" value="No" v-model="infectedQuestion" /> No
-    </div>
-    <input id="bubbles-three" type="text" v-model="treatment" placeholder="Treatment" />
-    <div class="form-questions">
-      <label for="admittedQuestion">Admitted?</label>
-      <input type="radio" id="yes" value="Yes" v-model="admittedQuestion" /> Yes
-      <input type="radio" id="no" value="No" v-model="admittedQuestion" /> No
-    </div>
-        <input id="bubbles-three" type="text" v-model="doctor" placeholder="Doctor Name" />
-    <button class="btn bubbles-four">Add</button>
-    </form>
-    
+          <div class="card-body">
+            <form @submit="addPatient">
+              <input id="bubbles-three" type="text" v-model="name" placeholder="Patient Name" />
+              <input
+                id="bubbles-three"
+                type="text"
+                v-model="phone_number"
+                placeholder="Phone Number"
+              />
+              <input type="text" class="form-control" v-model="city" placeholder="City" />
+              <input type="text" class="form-control" v-model="symptoms" placeholder="Symptoms" />
+              <div class="form-questions">
+                <label for="admittedQuestion">Infected?</label>
+                <input
+                  type="radio"
+                  class="form-control"
+                  id="yes"
+                  value="Yes"
+                  v-model="infectedQuestion"
+                /> Yes
+                <input
+                  type="radio"
+                  class="form-control"
+                  id="no"
+                  value="No"
+                  v-model="infectedQuestion"
+                /> No
+              </div>
+              <input id="bubbles-three" type="text" v-model="treatment" placeholder="Treatment" />
+              <div class="form-questions">
+                <label for="admittedQuestion">Admitted?</label>
+                <input
+                  type="radio"
+                  class="form-control"
+                  id="yes"
+                  value="Yes"
+                  v-model="admittedQuestion"
+                /> Yes
+                <input
+                  type="radio"
+                  class="form-control"
+                  id="no"
+                  value="No"
+                  v-model="admittedQuestion"
+                /> No
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                id="bubbles-three"
+                v-model="doctor"
+                placeholder="Doctor Name"
+              />
+              <button class="btn bubbles-four">Add</button>
+            </form>
+            <strong>Preview:</strong>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
 // Mounting Data
 export default {
-    mounted () {
-    console.log("Mount Success!")
-    },
+  mounted() {
+    console.log("Mount Success!");
+  },
 
-    data() {
-        return {
-            name: "",
-            phone_number: "",
-          city: "",
-          symptoms: "",
-          infectedQuestion: "",
-          treatment: "",
-          admittedQuestion: "",
-          doctor: "",
-          };
-    },
-    
+  data() {
+    return {
+      name: "",
+      phone_number: "",
+      city: "",
+      symptoms: "",
+      infectedQuestion: "",
+      treatment: "",
+      admittedQuestion: "",
+      doctor: ""
+    };
+  },
+
   methods: {
     addPatient(e) {
-        e.preventDefault();
-        let currentObj = this;
-      this.axios.post ("http://localhost:8081/api/patients")
-        .then(response => (
-          console.log("response: ",response),
-          this.info = response.data));
-          const searchData = this.info;
-          console.log("atx:",searchData);
-          const results = searchData.find( (patient) => {
-            return patient.name.toLowerCase() === this.search.toLowerCase();
-          });
-          this.patients = results;
-          // console.log("results: ", results);
-          // response.send(results);
+      e.preventDefault();
+      let currentObj = this;
+      this.axios
+        .post("http://localhost:8081/api/patients", {
+          name: this.name,
+          phone_number: this.phone_number,
+          city: this.city,
+          symptoms: this.symptoms,
+          infectedQuestion: this.infectedQuestion,
+          treatment: this.treatment,
+          admittedQuestion: this.admittedQuestion,
+          doctor: this.doctor
+        })
+        .then(function(response) {
+          currentObj.output = response.data;
+        })
+        .catch(function(error) {
+          currentObj.output = error;
+        });
     }
-
-
-
-
-    var patientString = new addPatient({
-      data: function(){
-      return {
-        addPatient: {
-          name: "",
-          phone_number: "",
-          city: "",
-          symptoms: "",
-          infectedQuestion: "",
-          treatment: "",
-          admitted: "",
-          doctor: "",
-          }
-          .then(
-          console.log(addPatient),
-          axios.post("http://localhost:8081/api/patient"))
-  created() {
-    axios.get("http://localhost:8081/api/patients").then(function(data) {
-      this.info = data.body.slice(0, 10);
-    });
   }
-      }
+};
 </script>
 
 <style>
@@ -117,10 +129,11 @@ export default {
 }
 
 li {
-  list-style-type: none
+  list-style-type: none;
 }
 
-td, th {
+td,
+th {
   padding-left: 15px;
 }
-
+</style>
