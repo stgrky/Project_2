716e3dc83d04159e416d6c8ee78aef407e6ec980
Leandra Turner <calliet0715@gmail.com>
2020-03-31 17:40:06 -0500

    Addition of background and centering elements

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,77 +1,95 @@
 <template id = "body">
-    <div id="login">
-        <h2>Doctor Login Portal</h2>
-        <input id="bubbles" type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input id="bubbles" type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button id="bubbles-two" type="button" v-on:click="login()">Login</button>
-    </div>
+  <div id="login">
+    <h2>C19 Health Portal</h2>
+    <img src=""
+    <input id="bubbles" type="text" name="username" v-model="input.username" placeholder="Username" />
+    <input
+      id="bubbles"
+      type="password"
+      name="password"
+      v-model="input.password"
+      placeholder="Password"
+    />
+    <button id="bubbles-two" type="button" v-on:click="login()">Login</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "login",
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            };
-        },
-        methods: {
-            login() {
-                if(this.input.username !== "" && this.input.password !== "") {
-                    if(this.input.username === this.$parent.mockAccount.username && this.input.password === this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
-        }
+export default {
+  name: "login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      }
     };
+  },
+  methods: {
+    login() {
+      if (this.input.username !== "" && this.input.password !== "") {
+        if (
+          this.input.username === this.$parent.mockAccount.username &&
+          this.input.password === this.$parent.mockAccount.password
+        ) {
+          this.$emit("authenticated", true);
+          this.$router.replace({ name: "secure" });
+        } else {
+          console.log("The username and / or password is incorrect");
+        }
+      } else {
+        console.log("A username and password must be present");
+      }
+    }
+  }
+};
 </script>
 
 <style>
-    #login {
-        width: 500px;
-        border: 1px solid #CCCCCC;
-        border-radius: 20px;
-        background-color: #FFFFFF;
-        margin: auto;
-        margin-top: 200px;
-        padding: 100px;
+body {
+  background: url(../assets/covid-bkgrd.jpg);
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
 
-    }
+h2 {
+  color: #bf9000;
+  text-align: center;
+}
 
-    #bubbles {
-        padding: 5px;
-        margin: 5px;
-        border-radius: 20px;
-        outline: none;
-    }
+#login {
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  border-radius: 20px;
+  width: 500px;
+  margin: 0 auto;
+  margin-top: 20%;
+  padding: 100px;
+}
 
-    #password {
-        padding: 5px;
-    }
+#bubbles {
+  border-radius: 20px;
+  outline: none;
+  display: block;
+  padding: 5px;
+  margin: 10px auto;
+}
 
-    #bubbles-two {
-        margin: 10px;
-        padding: 5px;
-        border-radius: 20px;
-        margin: 10px;
-        width: 100px;
-        outline: none;
-    }
+#password {
+  padding: 5px;
+}
 
-    #bubbles-two:hover{
-        background-color: lightblue;
-        outline: none;
+#bubbles-two {
+  padding: 5px;
+  border-radius: 20px;
+  display: block;
+  margin: 15px auto;
+  width: 100px;
+  outline: none;
+}
 
-    }
-
-
+#bubbles-two:hover {
+  background-color: #bf9000;
+  outline: none;
+}
 </style>
