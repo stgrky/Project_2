03288c04249033566c4f5bbd3783dd1c2a1fc4d0
Kendra Kwoka <kendrakwoka@gmail.com>
2020-03-31 16:47:22 -0500

    pushing to merge with GK

--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-const serveStatic = require("serve-static");
-const cors = require("cors");
 // const serveStatic = require("serve-static");
-const path = require("path");
+// const path = require("path");
 
 
 // Sets up the Express App
@@ -16,17 +14,12 @@ let PORT = process.env.PORT || 8081;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  console.log(req.url);
-  next();
-});
 
 // Requiring our models for syncing
 let db = require("./models");
 
 // Static directory
-app.use(serveStatic(path.join(__dirname + "/dist")));
+app.use(express.static("dist"));
 
 // Routes
 // =============================================================
@@ -36,8 +29,8 @@ require("./routes/patients-api-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
