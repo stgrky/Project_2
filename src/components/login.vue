c6d3f38bbf4eb001a77553909f4b354179f4e39a
Kendra Kwoka <kendrakwoka@gmail.com>
2020-04-01 08:03:59 -0500

    Created newUser page with 'create new login' button and it works!

--- src/components/login.vue
+++ src/components/login.vue
@@ -12,6 +12,8 @@
     />
     <button id="bubbles-two" type="button" v-on:click="login()">Login</button>
 
+    <router-link to="/signUp">Create New Login</router-link>
+
     <h6>A COVID-19 PATIENT DATABASE</h6> 
   </div>
 </template>
@@ -42,6 +44,9 @@ export default {
       } else {
         console.log("A username and password must be present");
       }
+    }, 
+    SignUp() {
+
     }
   }
 };
