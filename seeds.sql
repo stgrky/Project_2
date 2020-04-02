1820044417b77c90f8d6d9da38018b3441c89c1e
Grant Kyle <sgrantkyle@gmail.com>
2020-04-01 20:46:27 -0500

    push so i can pull

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,14 @@
-insert into Patients (name, phone_number, city, symptoms, infected, treatment, admitted, doctor)
-values ("Paul Schneck", 5128888888, "houston", "dry coughs", true, "medicine", true, "Dr. Snider");
+insert into Patients (name, lastName, phone_number, city, symptoms, infected, treatment, admitted, doctor)
+values ("Paul", "Schneck", 5128888888, "houston", "dry coughs", true, "medicine", true, "Dr. Snider");
 
-insert into Patients (name, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
-values ("Tyler", 5128884125, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
+insert into Patients (name, lastName, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
+values ("Tyler", "Guy", 5128884125, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
 
-insert into Patients (name, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
-values ("Leslie", 5128880000, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
+insert into Patients (name, lastName, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
+values ("Leslie", "Krapschtein", 5128880000, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
 
-insert into Patients (name, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
-values ("Adam", 5127897894, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
+insert into Patients (name, lastName, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
+values ("Adam", "The Rat", 5127897894, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
 
-insert into Patients (name, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
-values ("Kramer", 5120000000, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
+insert into Patients (name, lastName, phone_number, city, symptoms, infected, treatment, admitted, doctor, createdAt, updatedAt)
+values ("Cosmo", "Kramer", 5120000000, "houston", "dry coughs", true, "medicine", true, "Dr. Snider", "2018-12-19 09:26:03", "2018-12-20 09:26:03");
